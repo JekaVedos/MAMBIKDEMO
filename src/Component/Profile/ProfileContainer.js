83dadf99ce3c8c64ba3c63ad084c3e85e1b7e106
JekaVedos <81905677+JekaVedos@users.noreply.github.com>
2021-07-19 11:29:22 +0300

    Update ProfileContainer.js

--- src/Component/Profile/ProfileContainer.js
+++ src/Component/Profile/ProfileContainer.js
@@ -42,7 +42,7 @@ let mapStateToProps = (state) => {
 export default compose(
    connect(mapStateToProps, {getUserProfile, getStatus, updateStatus}),
    withRouter,
-   withAuthRedirect
+   // withAuthRedirect
 )(ProfileContainer)
 
 
